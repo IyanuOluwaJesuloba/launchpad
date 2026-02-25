3b66f39179d99aee30c4a0cef3a340de3c6c7fa8
Precious Akpan <precious.akpan2000@gmail.com>
2026-02-25 10:14:43 +0100

    test: add polyfills for TextEncoder and TextDecoder

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,5 +1,14 @@
-// Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import { TextEncoder, TextDecoder } from "util";
+
+if (typeof global.TextEncoder === "undefined") {
+  global.TextEncoder = TextEncoder;
+}
+
+if (typeof global.TextDecoder === "undefined") {
+  global.TextDecoder = TextDecoder;
+}
+
 
 // Mock window.matchMedia
 Object.defineProperty(window, "matchMedia", {
